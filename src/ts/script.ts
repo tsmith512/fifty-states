d5fb7e75597b0c56e0fc131f1608d07712ef1f6f
Taylor Smith <tsmith@tsmithcreative.com>
2025-12-02 00:43:35 -0600

    Initial sketch

---
+++ src/ts/script.ts
@@ -0,0 +1,21 @@
+interface State {
+  abbr: string;
+  days: number;
+  first: number;
+  points: number;
+  state: string;
+}
+
+(async () => {
+  const states = await fetch('https://api.routenotfound.com/fifty_states').then(r => r.json()) as State[];
+  const counter = document.querySelector('#count') as Element;
+
+  states.sort((a, b) => a.first - b.first);
+
+  for (let i = 0; i < states.length; i++) {
+    const state = states[i];
+    counter.textContent = `${i + 1}`;
+    document.querySelector(`#map path[id*="${state.abbr}"]`)?.classList.add('visited');
+    await new Promise(resolve => setTimeout(resolve, 125));
+  }
+})();
