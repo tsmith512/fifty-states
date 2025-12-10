0e413de1d3b23e0b5cd06b7803dc99a97c86139b
Taylor Smith <tsmith@tsmithcreative.com>
2025-12-10 17:28:40 -0600

    Add sometihng else silly

--- src/ts/script.ts
+++ src/ts/script.ts
@@ -10,14 +10,33 @@ interface State {
   const states = await fetch('https://api.routenotfound.com/fifty_states').then(r => r.json()) as State[];
   const counter = document.querySelector('#count') as Element;
 
-  states.sort((a, b) => a.first - b.first);
+  // quickly mark top 5 states
+  states.sort((a, b) => b.points - a.points);
+  const top5list = document.getElementById('states-list');
+  for (let i = 1; i < 6; i++) { // Skip home state
+    const state = states[i];
+    console.log(state)
+    if (top5list) {
+      const stateLi = document.createElement('li');
+      stateLi.textContent = state.state;
+      top5list.append(stateLi);
+    }
+    document.querySelectorAll(`#map [id^="${state.abbr}"]`).forEach(el => el.classList.add('top5'));
+  }
 
+
+  // Animate in the visited states, ordered by first waypoint
+  states.sort((a, b) => a.first - b.first);
   for (let i = 0; i < states.length; i++) {
     const state = states[i];
-    console.log(state.abbr);
+    // Mark the state visited (CSS will animate its opacity fade-in)
+    document.querySelectorAll(`#map [id^="${state.abbr}"]`).forEach(el => el.classList.add('visited'));
+
+    // Fill the text container that counts them
     counter.textContent = `${i + 1}`;
-    document.querySelectorAll(`#map [id*="${state.abbr}"]`).forEach(el => el.classList.add('visited'));
     document.getElementById('progress')?.style.setProperty('--progress', (i / 50) * 100);
+
+    // Delay for drama
     await new Promise(resolve => setTimeout(resolve, 125));
   }
 })();
